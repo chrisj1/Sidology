a14cce007f366e4dfaebb1c448074fe91308a2de
chrisj1 <cjerrett18@stjohnsprep.org>
2015-05-31 18:01:19 -0400

    Revert "Completed Launch Wrapper and Set up Camera"
    This reverts commit 6cc6c7f6ce643907ec8f652b7581ff50b85da6b7.

--- Sidstone/src-desktop/com/fizzion/sidstone/desktop/DesktopLauncher.java
+++ Sidstone/src-desktop/com/fizzion/sidstone/desktop/DesktopLauncher.java
@@ -6,20 +6,9 @@ import com.fizzion.sidstone.Game;
 
 public class DesktopLauncher {
 	
-	private static final String TITLE = "Sidstone";
-	
-	private static final int WIDTH = 1600;
-	private static final int HEIGHT = 900;
-	private static final int FPS = 60;
-	
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-		cfg.title = TITLE;
-		cfg.width = WIDTH;
-		cfg.height = HEIGHT;
-		cfg.backgroundFPS = FPS;
-		cfg.foregroundFPS = FPS;
-		new LwjglApplication(new Game(), cfg);
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
 	}
 	
 }
