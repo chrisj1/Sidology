888a9ade9a6b760eb873c614c8d38b6a30c8226b
Xydium <prodriver54@gmail.com>
2015-06-01 22:52:08 -0400

    Final Changes 6/1/15

--- Sidstone/src-core/com/fizzion/sidstone/screens/MainMenuScreen.java
+++ Sidstone/src-core/com/fizzion/sidstone/screens/MainMenuScreen.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.fizzion.sidstone.Application;
 
@@ -27,21 +28,14 @@ public class MainMenuScreen implements Screen {
 	public void render(float delta) {
 		clearScreen();
 		update(delta);
-		drawDebugText();
 	}
 	
 	private void update(float delta) {
 		
 	}
 	
-	private void drawDebugText() {
-		app.batch.begin();
-		app.batch.draw(app.assets.get("img/test.png", Texture.class), 0, 0);
-		app.batch.end();
-	}
-	
 	private void clearScreen() {
-		Gdx.gl.glClearColor(0.2f, 0f, 0.2f, 1);
+		Gdx.gl.glClearColor(0f, 0f, 0f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 	}
 
@@ -63,6 +57,7 @@ public class MainMenuScreen implements Screen {
 
 	@Override
 	public void dispose() {
+		stage.dispose();
 	}
 
 }
